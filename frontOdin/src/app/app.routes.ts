18e64d21c54b212febb1022124674976ba6e205a
Brandon Quintero <121878425+branr9@users.noreply.github.com>
2026-09-23 12:07:03 -0500

    enrutamiento
    se enrutan los modulos

--- frontOdin/src/app/app.routes.ts
+++ frontOdin/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterOutlet } from '@angular/router';
+import { Radicaion } from './radicaion/radicaion';
+import { GestionUsuarios } from './gestion-usuarios/gestion-usuarios';
+import { GestionTramites } from './gestion-tramites/gestion-tramites';
+import { ConsultaSeguimiento } from './consulta-seguimiento/consulta-seguimiento';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', redirectTo: 'radicacion', pathMatch: 'full' },
+{ path: 'radicacion', component: Radicaion },
+{ path: 'gestionusuarios', component: GestionUsuarios },
+{ path: 'gestiontramites', component: GestionTramites },
+{ path: 'consultaseguimiento', component: ConsultaSeguimiento },
+];
